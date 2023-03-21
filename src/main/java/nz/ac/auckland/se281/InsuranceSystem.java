f2d7b62dfba5b97011c9f9635f5bfbf46576c2ae
Sandie Seaforth <sandie.seaforth@gmail.com>
2023-03-22 01:24:22 +1300

    Figured how to create array and passes T 1.02

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,22 +1,36 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
 
-  public InsuranceSystem() {
+  ArrayList database = new ArrayList<>();
 
+  public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    System.out.println("Database has 0 profiles.");
+    if (database.size() == 0) {
+      System.out.println("Database has 0 profiles.");
+    }
+    if (database.size() == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+    }
     // MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
     // MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
   }
 
   public void createNewProfile(String userName, String age) {
-    // MessageCli.PROFILE_CREATED.printMessage(userName, age);
+
+    if (userName.length() < 3) {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    } else {
+      database.add(userName);
+      MessageCli.PROFILE_CREATED.printMessage(userName, age);
+    }
+
     // TODO: Complete this method.
   }
 
