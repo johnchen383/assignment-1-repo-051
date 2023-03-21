5cff5639164e04c77ee90b8fbbcf1fffa527c51b
Sandie Seaforth <sandie.seaforth@gmail.com>
2023-03-21 23:22:43 +1300

    Made a new class file and started experimenting

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -3,19 +3,27 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+
   public InsuranceSystem() {
+
     // Only this constructor can be used (if you need to initialise fields).
   }
 
-  public void printDatabase() {
-    // TODO: Complete this method.
+  public void printDatabase(String userName, String age) {
+    if (userName.length() == 0) {
+      System.out.println("Database has 0 profiles.");
+    }
+    MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+    // MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
   }
 
   public void createNewProfile(String userName, String age) {
+    MessageCli.PROFILE_CREATED.printMessage(userName, age);
     // TODO: Complete this method.
   }
 
   public void loadProfile(String userName) {
+
     // TODO: Complete this method.
   }
 
